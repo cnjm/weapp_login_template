00b3e8ed36f957b3ca5f267cefc85cad25a05244
szmfcjm_JfT <chenjiaming@shuzimofang.net>
2022-05-19 14:14:03 +0800

    fix

--- utils/login.js
+++ utils/login.js
@@ -4,29 +4,29 @@
  * 尽可能以 session_key 有效期作为自身登录态有效期
  * 这里默认后端缓存sesion_key,每次进入小程序前端会监测是否过期， 重新登录后端就可以刷新sesion_key
  */
-import {
-  Authorization, resResult
-} from '../network/config';
-import {login} from '../network/index.js';
+import { Authorization, resResult } from "../network/config";
+import { login } from "../network/index.js";
 
 /**
  * 用户登录逻辑,首次进入小程序或任意时刻重新登录
  */
 export const userLogin = () => {
-  return new Promise((resolve, reject)=>{
-    checkLogin().then((token) => {
-      console.log('old_token ====', token);
-    }).catch(async (err) => {
-      try {
-        const token = await refreshLogin();
-        console.log('new_token：', token);
-        resolve(token);
-      } catch (err) {
-        reject(err);
-      }
-    });
-  })
-}
+  return new Promise((resolve, reject) => {
+    checkLogin()
+      .then((token) => {
+        console.log("old_token ====", token);
+      })
+      .catch(async (err) => {
+        try {
+          const token = await refreshLogin();
+          console.log("new_token：", token);
+          resolve(token);
+        } catch (err) {
+          reject(err);
+        }
+      });
+  });
+};
 
 /**
  * 用户进入小程序静默登录前监测登录态
@@ -42,16 +42,16 @@ export const checkLogin = () => {
         if (token) {
           resolve(token);
         } else {
-          reject('token_err');
+          reject("token_err");
         }
       },
       fail() {
         // session_key 已经失效，需要重新执行登录流程
-        reject('session_key_err');
-      }
-    })
-  })
-}
+        reject("session_key_err");
+      },
+    });
+  });
+};
 
 /**
  * 重新登录======token不存在或过期 session_key失效时
@@ -63,19 +63,21 @@ export const refreshLogin = () => {
   return new Promise(async (resolve, reject) => {
     try {
       const code = await getWxCode();
-      login({code}).then((data)=>{
-        let token = data[resResult];
-        wx.setStorageSync(Authorization, token);
-        app.globalData.LOGIN_SWITCH = false;
-        resolve(token);
-      }).catch((err)=>{
-        reject(err)
-      })
+      login({ code })
+        .then((data) => {
+          let token = data[resResult];
+          wx.setStorageSync(Authorization, token);
+          app.globalData.LOGIN_SWITCH = false;
+          resolve(token);
+        })
+        .catch((err) => {
+          reject(err);
+        });
     } catch (err) {
-      reject(err)
+      reject(err);
     }
-  })
-}
+  });
+};
 
 /**
  * 获取 小程序的code
@@ -88,12 +90,12 @@ export const getWxCode = () => {
           resolve(res.code);
         } else {
           wx.showToast({
-            title: '登录失败，请重新进入小程序',
-            icon: 'none',
-            duration: 3000
-          })
+            title: "登录失败，请重新进入小程序",
+            icon: "none",
+            duration: 3000,
+          });
         }
-      }
-    })
-  })
-}
+      },
+    });
+  });
+};
